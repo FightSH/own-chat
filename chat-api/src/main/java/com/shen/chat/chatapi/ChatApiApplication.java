164f85bba0b631a392168ea7fef3864ff9b87583
shen <shenhao189@foxmail.com>
2024-04-09 00:02:53 +0800

    second commit

---
+++ chat-api/src/main/java/com/shen/chat/chatapi/ChatApiApplication.java
@@ -0,0 +1,43 @@
+package com.shen.chat.chatapi;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class ChatApiApplication {
+
+
+
+    private Logger logger = LoggerFactory.getLogger(ChatApiApplication.class);
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApiApplication.class, args);
+    }
+
+
+    @GetMapping("/verify")
+    public ResponseEntity<String> verify(String token) {
+        logger.info("验证 token：{}", token);
+        if ("success".equals(token)){
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @GetMapping("/success")
+    public String success(){
+        return "test success by shen";
+    }
+
+}
